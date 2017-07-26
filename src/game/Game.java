72cd7a4ad0337a31fb594392f828e39a5c3f4d23
ladislavborbely <ladislavborbely@gmail.com>
2017-07-26 18:18:10 +0200

    new upd

--- src/game/Game.java
+++ src/game/Game.java
@@ -19,46 +19,44 @@ public class Game {
 	private Deck deck = new Deck();
 	private int round = 1;
 
+	/**
+	 * Run the game
+	 */
 	public void run() {
 		AiSimple ai = new AiSimple(playerHandPc);
 		deck.generate();
 		deck.shuffle();
-		dealFirstRound();
 
-		System.out.println("══════════════════════════ End of round " + round++ + "══════════════════════════");
+		dealFirstRound();
+		printEndOfTurn();
 
 		while (playerHandHuman.getValue() < 22 && playerHandPc.getValue() < 22) {//len skuska ci funguju veci
-
-			System.out.println("\"n\" for next card else for stop ...");
-			if (sc.nextLine().equals("n")) {
-				System.out.println(playerHandHuman.getHandOwnerName() + ": More please.");
-				giveCardTo(playerHandHuman);
-				System.out.println();
-			} else {
-				System.out.println(playerHandHuman.getHandOwnerName() + ": Enough.");
-				playerHandHuman.showHandAdvanced();
-				System.out.println();
-			}
-
-			System.out.println();
-			System.out.println("Ai turn...");
-			if (ai.wantNextCard()){
-				System.out.println(playerHandPc.getHandOwnerName() + ": More please.");
-				giveCardTo(playerHandPc);
-				System.out.println();
-			} else {
-				System.out.println(playerHandPc.getHandOwnerName() + ": Enough.");
-				playerHandPc.showHandAdvanced();
-				System.out.println();
-			}
-
-			System.out.println("══════════════════════════ End of round " + round++ + "══════════════════════════");
+			doesPlayerWantAcard(playerChoice(), playerHandHuman);
+			doesPlayerWantAcard(ai.wantNextCard(), playerHandPc);
+			printEndOfTurn();
 		}
 		System.out.println("More than 22!");
 	}
 
-	private void doesPlayerWantAcard(boolean next, Hand hand) {
-		if (next) { //next card = true
+	private boolean playerChoice() {
+		System.out.println("Press \"n\" for next card, any other key stop ...");
+		if (sc.nextLine().equals("n")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * If a player wants a card then its added to the "his" hand.
+	 * 
+	 * @param nextCard
+	 *            true for next card, false for stop +lock the hand
+	 * @param hand
+	 *            in case of nextCard = true add a card to this hand
+	 */
+	private void doesPlayerWantAcard(boolean nextCard, Hand hand) {
+		if (nextCard) { //next card = true
 			System.out.println(playerHandPc.getHandOwnerName() + ": More please.");
 			giveCardTo(hand);
 		} else { // no next card
@@ -68,16 +66,29 @@ public class Game {
 
 	}
 
+	/**
+	 * Print end of turn line, visual feature.
+	 */
+	private void printEndOfTurn() {
+		System.out.println("══════════════════════════ End of round " + round++ + "══════════════════════════");
+	}
+
+	/**
+	 * Give a card to each player
+	 */
 	private void dealFirstRound() {
 		giveCardTo(playerHandHuman);
 		giveCardTo(playerHandPc);
 	}
 
+	/**
+	 * Give a card to hand and show the cards in hand + values
+	 * 
+	 * @param hand
+	 */
 	private void giveCardTo(Hand hand) {
 		hand.addCard(deck.getNextCard());
 		hand.showHandAdvanced();
 		System.out.println("Player: " + hand.getHandOwnerName() + " Total:" + hand.getValue());
 	}
-
-	// turn()
 }
