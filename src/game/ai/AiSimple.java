de0c1922f0e00db26324388ce19a1727625334d2
ladislavborbely <ladislavborbely@gmail.com>
2017-07-26 18:27:22 +0200

    Ai upd

--- src/game/ai/AiSimple.java
+++ src/game/ai/AiSimple.java
@@ -3,13 +3,12 @@ package game.ai;
 import game.Hand;
 
 /**
- * Simple computer player if hand value < 14 ask for another card
+ * Simple computer player if hand value < 15 ask for another card
  * 
  * @author Ladislav Borbely
  * @version 0.1 (23. 7. 2017)
  *
  */
-
 
 public class AiSimple {
 	private Hand hand;
@@ -19,7 +18,7 @@ public class AiSimple {
 	}
 
 	public boolean wantNextCard() {
-		if (hand.getValue() < 14) {
+		if (hand.getValue() < 15) {
 			return true;
 		} else {
 			return false;
